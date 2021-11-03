74f8369cde2be05c8bb553aac22104ad819ee03e
Diego Minelli <diego.minelli@hotmail.com>
2021-11-03 16:51:21 -0300

    Fazendo o partial application

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,12 +1,13 @@
 import { handleStatus } from '../utils/promise-helpers.js';
+import { partialize } from '../utils/operators.js';
 
 const API = 'http://localhost:3000/notas';
 
-const sumItems = (code) => (notas) =>
-  notas
-    .$flatMap((nota) => nota.itens)
-    .filter((item) => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = (notas) => notas.$flatMap((nota) => nota.itens);
+const filterItemsByCode = (code, items) =>
+  items.filter((item) => item.codigo == code);
+const sumItemsValue = (items) =>
+  items.reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
   listAll() {
@@ -14,6 +15,8 @@ export const notasService = {
   },
 
   sumItems(code) {
+    const filterItems = partialize(filterItemsByCode, code);
+
     return this.listAll().then(sumItems(code));
   },
 };
