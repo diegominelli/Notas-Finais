ba77637dad9fbc7bfcadb91830f48b5f667cb15d
Diego Minelli <diego.minelli@hotmail.com>
2021-11-04 18:52:26 -0300

    Lidando com dados nulos

--- public/app/app.js
+++ public/app/app.js
@@ -2,20 +2,30 @@
 import { log, timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js';
+import {
+  takeUntil,
+  debounceTime,
+  partialize,
+  pipe,
+} from './utils/operators.js';
 import { EventEmitter } from './utils/event-emitter.js';
+import { Maybe } from './utils/maybe.js';
+
+const value = Maybe.of(20)
+  .map((value) => value + 10)
+  .map((value) => value + 30)
+  .getOrElse(0);
+alert(value);
 
 const operations = pipe(
-    partialize(takeUntil, 3),
-    partialize(debounceTime, 500)
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500)
 );
 
-const action = operations(() => 
-    retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
-    .then(total => EventEmitter.emit('itensTotalizados', total))
+const action = operations(() =>
+  retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
+    .then((total) => EventEmitter.emit('itensTotalizados', total))
     .catch(console.log)
 );
 
-document
-.querySelector('#myButton')
-.onclick = action;
+document.querySelector('#myButton').onclick = action;
--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -3,32 +3,27 @@ import { partialize, pipe } from '../utils/operators.js';
 
 const API = 'http://localhost:3000/notas';
 
-const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
-const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
-const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = (notas) => notas.$flatMap((nota) => nota.itens);
+const filterItemsByCode = (code, items) =>
+  items.filter((item) => item.codigo == code);
+const sumItemsValue = (items) =>
+  items.reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
+  listAll() {
+    return fetch(API)
+      .then(handleStatus)
+      .catch((err) => {
+        console.log(err);
+        return Promise.reject('Não foi possível obter as notas fiscais');
+      });
+  },
 
-    listAll() {
+  sumItems(code) {
+    const filterItems = partialize(filterItemsByCode, code);
 
-        return fetch(API)
-            .then(handleStatus)
-            .catch(err => {
-                console.log(err);
-                return Promise.reject('Não foi possível obter as notas fiscais');
-            });
-    },
+    const sumItems = pipe(getItemsFromNotas, filterItems, sumItemsValue);
 
-    sumItems(code) {
-        const filterItems = partialize(filterItemsByCode, code);
-        
-        const sumItems = pipe(
-            getItemsFromNotas, 
-            filterItems, 
-            sumItemsValue
-        );
-
-        return this.listAll()
-            .then(sumItems);
-    }
+    return this.listAll().then(sumItems);
+  },
 };
