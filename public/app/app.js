69d85a999e7269b5a8d947f902836bfeb1eb2228
Diego Minelli <diego.minelli@hotmail.com>
2021-11-04 11:49:33 -0300

    Implementando timeout em Promise

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,21 @@
-import { log } from './utils/promise-helpers.js';
+import { log, timeoutPromise } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil, debounceTime } from './utils/operators.js';
+import {
+  takeUntil,
+  debounceTime,
+  partialize,
+  pipe,
+} from './utils/operators.js';
 
-const action = debounceTime(
-  500,
-  takeUntil(3, () =>
+const operations = pipe(
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500)
+);
+
+const action = operations(() =>
+  timeoutPromise(
+    200,
     service.sumItems('2143').then(console.log).catch(console.log)
   )
 );
