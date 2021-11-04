0743260b33f85ddafe716448e9939d595908658b
Diego Minelli <diego.minelli@hotmail.com>
2021-11-04 18:09:22 -0300

    Fazendo o alto acoplamento

--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,4 @@
+// app/app.js
 import { log, timeoutPromise, retry } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
 import { notasService as service } from './nota/service.js';
@@ -5,12 +6,12 @@ import { takeUntil, debounceTime, partialize, pipe } from './utils/operators.js'
 import { EventEmitter } from './utils/event-emitter.js';
 
 const operations = pipe(
-  partialize(takeUntil, 3),
-  partialize(debounceTime, 500)
+    partialize(takeUntil, 3),
+    partialize(debounceTime, 500)
 );
 
-const action = operations(() =>
-  retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
+const action = operations(() => 
+    retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
     .then(total => EventEmitter.emit('itensTotalizados', total))
     .catch(console.log)
 );
